431d956754e3b5ce91e25ba99beec8f2964b4c66
Yangjinho <yangjinho826@forelink.co.kr>
2022-05-23 16:41:38 +0900

    퀴즈를 맞춰야 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path
 from . import views
+from .forms import LoginForm
 
 # app_name = 'accounts'
 
 urlpatterns = [
     # path('login/', views.account_login, name='login'),
-    path('login/', LoginView.as_view(template_name='accounts/login_form.html'), name='login'),
+   path('login/', LoginView.as_view(
+        form_class=LoginForm,
+        template_name='accounts/login_form.html'
+    ), name='login'),
+    path('logout/', views.logout, name='logout'),
     path('profile/', views.profile, name="profile"),
     path('profile/edit/', views.profile_edit, name="profile_edit"),
+    path('signup/', views.signup, name='signup'),
 ]
