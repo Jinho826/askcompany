a20bcf1cb833081f339a4c37189d4d644eb9bf3f
Yangjinho <yangjinho826@forelink.co.kr>
2022-05-23 17:16:52 +0900

    자동로그인

--- accounts/views.py
+++ accounts/views.py
@@ -32,6 +32,7 @@
 # account_login = LoginCustom.as_view()
 
 
+from urllib import response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
@@ -40,7 +41,7 @@ from django.shortcuts import render, redirect
 from accounts.forms import ProfileForm
 from accounts.models import Profile
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login as auth_login
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
@@ -77,12 +78,20 @@ def profile_edit(request):
 
 User = get_user_model()
 
-signup = CreateView.as_view(
-    model = User,
-    form_class=UserCreationForm,
-    success_url=settings.LOGIN_URL,
-    template_name='accounts/signup_form.html',
-)
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = settings.LOGIN_REDIRECT_URL
+    template_name = 'accounts/signup_form.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.object
+        auth_login(self.request, user)
+        return response
+
+
+signup = SignupView.as_view()
 
 
 def logout(request):
